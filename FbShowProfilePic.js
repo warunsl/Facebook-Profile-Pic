5cade92bcad61b46dc4f6b976c7a6de998ba9365
Varun S Lingaraju <warunsl@gmail.com>
2013-12-25 16:12:58 -0800

    Removed lightbox dependencies.

--- FbShowProfilePic.js
+++ FbShowProfilePic.js
@@ -1,56 +1,47 @@
 (function () {
-    var smallImageObject;
-    var newImageUrl;
-    var converted=false;
+    converted=false;
 
-    getPicUrl();
-
-    function getPicUrl() {
-        
+    getPic=function() {
         if(converted) return;
 
-        smallImageObject = $(".profilePic")
-        var smallImageUrl = smallImageObject.attr('src');
-        var smallImageUrlList = smallImageUrl.split('/');
-        var newImageUrlList = new Array();
-
-        $.each(smallImageUrlList , function( index, value ) {
-            if(index!=4 && index!=5) {
-                newImageUrlList.push(value)
-                newImageUrlList.push("/")
-            }
-        });
-
-        newImageUrl = "";
-        $.each(newImageUrlList, function( index, value ){
-            newImageUrl = newImageUrl + value;
-        });
-
-        newImageUrl = newImageUrl.substring(0, newImageUrl.length - 1);
-        console.log(newImageUrl);
-
-        smallImageObject.attr('src', newImageUrl);
+        // Compute the new URL which has the enlarged pic
+        var b=["https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn1/","https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn2/","https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn3/","https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn4/","https://scontent-a-lax.xx.fbcdn.net/hphotos-frc1/","https://scontent-a-lax.xx.fbcdn.net/hphotos-frc2/","https://scontent-a-lax.xx.fbcdn.net/hphotos-frc3/","https://scontent-a-lax.xx.fbcdn.net/hphotos-frc4/","https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-ash1/","https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-ash2/","https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-ash3/","https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-ash4/"];
+        n=$(".profilePic")[0]["src"].lastIndexOf("/");
+        random=Math.floor((Math.random()*11));
+        person_img=$(".profilePic")[0]["src"].substr(n+1);
+        final_url=b[random]+person_img;
 
         converted=true;
-    }
+        lightbox(final_url);
+    };
 
-    smallImageObject.on('click', function() {
-        smallImageObject.attr('data-lightbox', newImageUrl);        
-    })
+    // Code to display the enlarged pic
+    lightbox=function(newImageUrl) {
 
-    function loadfile(filename, filetype){
-        if (filetype=="js"){
-            var fileref=document.createElement('script')
-            fileref.setAttribute("type","text/javascript")
-            fileref.setAttribute("src", filename)
-        }
-        else if (filetype=="css"){
-            var fileref=document.createElement("link")
-            fileref.setAttribute("rel", "stylesheet")
-            fileref.setAttribute("type", "text/css")
-            fileref.setAttribute("href", filename)
-        }
-        if (typeof fileref!="undefined")
-            document.getElementsByTagName("head")[0].appendChild(fileref)
-    }
+        aElement = document.createElement('a');
+        aElement.setAttribute('rel', 'lightbox');
+        aElement.setAttribute('class', 'profilePicThumb');
+        aElement.setAttribute('id', 'profile_pic_education');
+        aElement.setAttribute('href', newImageUrl);
+        aElement.click(function() {
+            var imageLink = $(this).find("a.profilePicThumb");
+            imageLink.attr("target", "_blank");
+            window.open(imageLink.attr("href"));
+            return false;
+        });
+
+        imgElementNew = document.createElement('img');
+        imgElementNew.setAttribute('class', 'profilePic img')
+        imgElementNew.setAttribute('src', newImageUrl);
+
+        aElement.appendChild(imgElementNew);
+
+        photoContainerElement = document.getElementsByClassName('photoContainer')[0];
+        photoContainerElementFirstChild = photoContainerElement.childNodes[0];
+        photoContainerElement.replaceChild(aElement, photoContainerElementFirstChild);
+
+    };
+
+    getPic();
+
 })();
